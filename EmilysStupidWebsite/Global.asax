ea7d7cff95cab992db8f3e2aa243f20249b2ef37
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2018-03-27 14:36:09 -0600

    Add project files.

---
+++ EmilysStupidWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmilysStupidWebsite.Global" Language="C#" %>
---
+++ EmilysStupidWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmilysStupidWebsite.Startup))]
+namespace EmilysStupidWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
