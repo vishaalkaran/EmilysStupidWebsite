9b8994099a92e0336abe14a82372a546ed89782c
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2018-03-29 14:26:23 -0600

    Update

--- EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx
+++ EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx
@@ -2,23 +2,26 @@
 
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+ 
     <div class="page-header">
         <h1>Vincent Karan New York</h1>
     </div>
 
     <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          One of three columns
+      <div class="col-6">
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
         </div>
-        <div class="col-sm">
-           <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>  
+      <div class="col-6">
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>  
             <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>  
             <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>  
             <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>  
-            <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>  
-        </div>
-        
+            <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
       </div>
     </div>
 
--- EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx.designer.cs
+++ EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx.designer.cs
@@ -13,6 +13,51 @@ namespace EmilysStupidWebsite {
     public partial class CreatePurchaseOrder {
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
         /// TextBox5 control.
         /// </summary>
         /// <remarks>
