4d0c7be771df8e693d5f1c8f25544a3a49a975db
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2018-03-29 16:45:14 -0600

    Update

--- EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx
+++ EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx
@@ -2,28 +2,45 @@
 
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
- 
-    <div class="page-header">
-        <h1>Vincent Karan New York</h1>
-    </div>
-
     <div class="container">
-      <div class="col-6">
-            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+        <%--<div class="page-header">
+            <h1>Vincent Karan New York</h1>
+        </div>--%>
+        
+        <div class="page-header">
+            <h1>Supplier Purchase Orders</h1>
         </div>
-      <div class="col-6">
-            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>  
-            <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>  
-            <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>  
-            <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>  
-            <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
-      </div>
-    </div>
 
-    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4" >
+                </div>
+                <div class="col-md-1" >
+                    <asp:Label ID="Label11" runat="server" Text="Quantity:" style="text-align: left;"></asp:Label>
+                    <asp:Label ID="Label12" runat="server" Text="Fabric:" style="text-align: right;"></asp:Label>
+                    <asp:Label ID="Label13" runat="server" Text="Pattern:" style="text-align: right;" ></asp:Label>
+                    <asp:Label ID="Label14" runat="server" Text="Size:" style="text-align: right;"></asp:Label>
+                    
+                    <asp:Label ID="Label15" runat="server" Text="Thread:" style="text-align: right;"></asp:Label>
+                    <asp:Label ID="Label41" runat="server" Text="Decals:" style="text-align: right;"></asp:Label>
+                    <asp:Label ID="Label1" runat="server" Text="Button:" style="text-align: right;"></asp:Label>
+                    <asp:Label ID="Label2" runat="server" Text="Zippers:" style="text-align: right;"></asp:Label>
+                    
+                    <asp:Label ID="Label43" runat="server" Text="Comments:" style="text-align: right;"></asp:Label>
+                </div>
+                <div class="col-md-1">
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>  
+                   
+                    <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>  
+                    <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox> 
+                </div>
+            </div>
+        </div>
+        
+    </div>
 </asp:Content>
--- EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx.designer.cs
+++ EmilysStupidWebsite/SimCard/CreatePurchaseOrder.aspx.designer.cs
@@ -13,6 +13,60 @@ namespace EmilysStupidWebsite {
     public partial class CreatePurchaseOrder {
         
         /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+        
+        /// <summary>
+        /// Label12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+        
+        /// <summary>
+        /// Label13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+        
+        /// <summary>
+        /// Label14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+        
+        /// <summary>
+        /// Label15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+        
+        /// <summary>
+        /// Label41 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label41;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -31,31 +85,13 @@ namespace EmilysStupidWebsite {
         protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
-        /// Label3 control.
+        /// Label43 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-        
-        /// <summary>
-        /// Label4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
-        
-        /// <summary>
-        /// Label5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label5;
+        protected global::System.Web.UI.WebControls.Label Label43;
         
         /// <summary>
         /// TextBox5 control.
@@ -65,6 +101,33 @@ namespace EmilysStupidWebsite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
         
         /// <summary>
         /// TextBox6 control.
